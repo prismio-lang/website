f01ec94d4d0970bf7b8907752cbe805e30423011
saksham1319 <saksham6975@gmail.com>
2026-09-14 00:42:12 +0530

    JsonLD

--- apps/web/config/structured-data.ts
+++ apps/web/config/structured-data.ts
@@ -1,40 +1,40 @@
 export const PRISMIO_ORG_ID =
-    "https://prismio.org";
+  "https://prismio.org/#organization";
 
 export const SAKSHAM_PERSON_ID =
-    "https://prismio.org/team/saksham-jaiswal/";
+  "https://prismio.org/team/saksham-jaiswal/#person";
 
 export const prismioStructuredData = {
-    "@context": "https://schema.org",
-    "@graph": [
-        {
-            "@type": "Organization",
-            "@id": PRISMIO_ORG_ID,
-            "name": "Prismio",
-            "url": "https://prismio.org/",
-            "description":
-                "Prismio is an open-source systems programming language that compiles to native machine code through LLVM.",
-            "founder": {
-                "@id": SAKSHAM_PERSON_ID
-            },
-            "sameAs": [
-                "https://github.com/prismio-lang/prismio"
-            ]
-        },
-        {
-            "@type": "Person",
-            "@id": SAKSHAM_PERSON_ID,
-            "name": "Saksham Jaiswal",
-            "url":
-                "https://prismio.org/team/saksham-jaiswal/",
-            "jobTitle": "Creator & Lead Developer",
-            "worksFor": {
-                "@id": PRISMIO_ORG_ID
-            },
-            "sameAs": [
-                "https://github.com/saksham1319",
-                "https://www.linkedin.com/in/saksham6975/"
-            ]
-        }
-    ]
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      "@id": PRISMIO_ORG_ID,
+      "name": "Prismio",
+      "url": "https://prismio.org/",
+      "description":
+        "Prismio is an open-source systems programming language that compiles to native machine code through LLVM.",
+      "founder": {
+        "@id": SAKSHAM_PERSON_ID
+      },
+      "sameAs": [
+        "https://github.com/prismio-lang/prismio"
+      ]
+    },
+    {
+      "@type": "Person",
+      "@id": SAKSHAM_PERSON_ID,
+      "name": "Saksham Jaiswal",
+      "url":
+        "https://prismio.org/team/saksham-jaiswal/",
+      "jobTitle": "Creator & Lead Developer",
+      "worksFor": {
+        "@id": PRISMIO_ORG_ID
+      },
+      "sameAs": [
+        "https://github.com/saksham1319",
+        "https://www.linkedin.com/in/saksham6975/"
+      ]
+    }
+  ]
 };
